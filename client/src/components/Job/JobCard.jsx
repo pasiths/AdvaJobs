8cc4396f9a868817e3d141caba34b3e08daa9409
Supun Chamuditha <3supun@gmail.com>
2024-11-30 20:50:36 +0530

    JobPage

---
+++ client/src/components/Job/JobCard.jsx
@@ -0,0 +1,28 @@
+import { Card, Row, Col } from 'react-bootstrap';
+
+const JobCard = ({ job }) => {
+  return (
+    <Card className="p-3">
+      <Row>
+        <Col md={2}>
+          <img
+            src="https://via.placeholder.com/50"
+            alt="company logo"
+            className="img-fluid"
+          />
+        </Col>
+        <Col md={10}>
+          <h6>{job.title}</h6>
+          <p className="mb-1">{job.company}</p>
+          <small>{job.timeAgo}</small>
+          <div className="d-flex justify-content-between mt-2">
+            <small>{job.location}</small>
+            <a href="/jobAdPage">View Details</a>
+          </div>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default JobCard;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Banner from '../components/Home/Banner';
+import BannerRec from '../components/Home/BannerRec';
+import JobPortalContent from '../components/Home/JobPortalContent';
+
+
+function HomePage() {
+  return (
+    <div>
+        <Banner/>
+        <BannerRec/>
+        <JobPortalContent />
+    </div>
+  )
+}
+
+export default HomePage
