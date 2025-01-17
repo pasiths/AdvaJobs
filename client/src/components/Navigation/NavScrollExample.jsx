7930d2e53127a4730b9c1c619191d05c2a3219dc
Pasith Senevirathna <pasith.senevi02@gmail.com>
2025-01-17 13:09:32 +0530

    Update cookie settings for auth token and refactor navigation component

---
+++ client/src/components/Navigation/NavScrollExample.jsx
@@ -0,0 +1,59 @@
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom"; // Import NavLink from react-router-dom
+import advajobs from "../../../src/assets/advajobs.svg";
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top shadow-sm">
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img
+            src={advajobs}
+            alt="Logo"
+            width="40"
+            height="40"
+            style={{ marginRight: "8px" }} // Adds spacing between the logo and text
+          />
+          AdvaJobs
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="mx-auto my-2 my-lg-0" navbarScroll>
+            {/* Use 'NavLink' instead of 'Nav.Link' */}
+            <Nav.Link as={NavLink} to="/jobs">
+              Jobs
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact Us
+            </Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            {/* Profile icon as an example */}
+            <Nav className="ms-auto">
+              <Button
+                as={NavLink}
+                to="/companylogin"
+                variant="outline-primary"
+                className="me-2"
+              >
+                Company Login
+              </Button>
+              <Button as={NavLink} to="/userlogin" variant="outline-success">
+                Job Seeker Login
+              </Button>
+            </Nav>
+            {/* <Nav.Link as={NavLink} to="/profile">
+              Profile
+              <i className="bi bi-person-circle ms-2" style={{ fontSize: '1.5rem' }}></i>
+            </Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
